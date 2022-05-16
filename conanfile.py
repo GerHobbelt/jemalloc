4f2f6dd70bdafcf6ada38bf54dc5e0da46d66547
hyeonwoo <hyeonwoo@gurum.cc>
2022-05-16 13:09:09 +0900

    Support conan build on Windows

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class JEMallocConan(ConanFile):
     name = "jemalloc"
-    version = "5.2.1"
+    version = "5.2.1-wintest"
     license = "Proprietary"
     url = "https://github.com/gurumnet/jemalloc"
     description = "General purpose malloc implementation"
@@ -14,15 +14,20 @@ class JEMallocConan(ConanFile):
     generators = "cmake"
 
     def source(self):
-        self.run("git clone https://github.com/gurumnet/jemalloc.git -b 5.2.1-1")
+        self.run("git clone https://github.com/gurumnet/jemalloc.git -b wintest")
 
     def build(self):
         os.chdir('jemalloc')
-        os.system('./autogen.sh --with-jemalloc-prefix=je_')
+        if os.name == "nt":
+            os.system('sh ./autogen.sh --with-jemalloc-prefix=je_')
+        else:
+            os.system('./autogen.sh --with-jemalloc-prefix=je_')
         os.system('make build_lib_static')
 
     def package(self):
         self.copy("*.a", dst="lib", src="jemalloc/lib", keep_path=False)
+        if os.name == "nt":
+            self.copy("*.lib", dst="lib", src="jemalloc/lib", keep_path=False)
         self.copy("*.h", dst="include", src="jemalloc/include", keep_path=True)
 
     def package_info(self):
